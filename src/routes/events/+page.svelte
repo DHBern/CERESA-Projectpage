3d9c3fd89f9f2d3c7fe29442a0ac613266bff374
vvvyyynet <levyn.buerki@unibe.ch>
2025-10-28 12:51:18 +0100

    feat: bilingual events page

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,29 +1,49 @@
+<script>
+	import { m } from '$lib/paraglide/messages';
+</script>
+
 <h1 class="h1">Attività</h1>
 
-<ul>
-	<li class="mb-5">
-		<span class="font-bold">12 maggio 2025</span><br />
-		Eva Vanacore<br />
-		Gender e genere negli inediti di Alice Ceresa<br />
-		Seminario dottorale Parole in viaggio: tradizione, innovazione e intermedialità, Università di Siviglia
+<ul class="eventlist">
+	<li class="mb-15">
+		<span class="date">{m.mean_lost_puffin_savor()}</span><br />
+		<span class="title">Gender e genere negli inediti di Alice Ceresa</span><br />
+		<span class="person">Eva Vanacore</span><br />
+		{@html m.mean_short_goat_coax()}<br />
+		{m.uni_siviglia()}
 	</li>
 
-	<li class="mb-5">
-		<span class="font-bold">12 giugno 2025</span><br />
-		Eleonora Norcini, Eva Vanacore<br />
-		Presentazione del progetto Arcipelago Ceresa<br />
-		Digital Editions Community Meeting, Zentrum Digitale Editionen, Università di Zurigo
+	<li class="mb-15">
+		<span class="date">{m.drab_fluffy_iguana_borrow()}</span><br />
+		<span class="title">{@html m.extra_weary_ladybug_promise()}</span><br />
+		<span class="person">Eleonora Norcini, Eva Vanacore</span><br />
+		Digital Editions Community Meeting, Zentrum Digitale Editionen<br />
+		{m.UZH()}
 	</li>
 
-	<li class="mb-5">
-		<span class="font-bold">13 ottobre 2025</span><br />
+	<li class="mb-15">
+		<span class="date">{m.aware_key_wallaby_walk()}</span><br />
+		<span class="title">{m.orange_swift_hyena_trim()}</span><br />
 		Workshop<br />
-		Presentazione e discussione sui prototipi
 	</li>
 
-	<li class="mb-5">
-		<span class="font-bold">6–7 marzo 2026</span><br />
-		Atelier numérique del progetto PARES<br />
-		Università di Zurigo (UZH)
+	<li class="mb-15">
+		<span class="date">{m.noble_game_jannes_dig()}</span><br />
+		<span class="title">{m.helpful_clear_gopher_cure()}</span><br />
+		{m.UZH()}
 	</li>
 </ul>
+
+<style>
+	@reference "tailwindcss";
+	.eventlist :global(.title) {
+		@apply text-2xl font-bold;
+	}
+
+	.eventlist :global(.date) {
+		@apply font-bold;
+	}
+	.eventlist :global(.person) {
+		@apply font-bold;
+	}
+</style>
