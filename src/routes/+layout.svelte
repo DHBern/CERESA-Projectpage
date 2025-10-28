f22f5d7755bbd380f9317c8cd27ecad7ac4409e4
vvvyyynet <levyn.buerki@unibe.ch>
2025-10-28 12:50:20 +0100

    feat: new images

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,7 @@
 		<a href={`${base}/.`}
 			><img
 				class="aspect-auto max-h-[40px] min-w-25"
-				src="/images/ceresa-firma.webp"
+				src="/images/ceresa-firma-transparent.webp"
 				alt="Signatura di Alice Ceresa"
 			/></a
 		>
@@ -34,7 +34,29 @@
 	<div class="mx-auto max-w-300 grow p-10 py-20">
 		{@render children?.()}
 	</div>
-	<div class="min-h-30 bg-surface-600-400 p-10">
-		<img class="h-20" src="/images/ceresa-firma.webp" alt="Signatura di Alice Ceresa" />
+	<div class="flex min-h-30 w-full flex-wrap gap-10 bg-surface-600-400 p-10">
+		<img
+			class="mx-auto h-20"
+			src="/images/ceresa-firma-transparent.webp"
+			alt="Signatura di Alice Ceresa"
+		/>
+	</div>
+	<div class="flex min-h-30 flex-wrap justify-around gap-10 bg-surface-600-400 p-10">
+		<div class="w-70 bg-white">
+			<img class="mx-auto h-20" src="/images/logo-unibe.webp" alt="Logo Universität Bern" />
+		</div>
+		<div class="w-70 bg-white">
+			<img class="mx-auto h-20" src="/images/logo-uzh.webp" alt="Logo Universität Zürich" />
+		</div>
+		<div class="w-70 bg-white">
+			<img class="mx-auto h-20" src="/images/logo-snf.webp" alt="Logo SNF" />
+		</div>
+		<div class="w-70 bg-white">
+			<img
+				class="mx-auto h-20"
+				src="/images/logo-ch.webp"
+				alt="Logo Schweizerische Eidgenossenschaft"
+			/>
+		</div>
 	</div>
 </div>
--- src/routes/ceresa/+page.svelte
+++ src/routes/ceresa/+page.svelte
@@ -3,4 +3,10 @@
 </script>
 
 <h1 class="h1">Alice Ceresa</h1>
-{@html m.ceresa_text()}
+<div class="flex flex-wrap">
+	{@html m.ceresa_text()}
+	<div class="mx-auto flex flex-col">
+		<img src="/images/ceresa-autoritratto-transparent.webp" alt="" class="mx-auto" />
+		<p class="text-xs">{m.bald_tame_sawfish_dig()}</p>
+	</div>
+</div>
